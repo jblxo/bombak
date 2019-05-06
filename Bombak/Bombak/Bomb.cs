034badede6ac7a51e836500159d9489342700de1
Ondřej Hliba <hliba.ondra@live.com>
2019-05-06 12:02:56 +0200

    Make Bomb and Runner class derive from Entity class

---
+++ Bombak/Bombak/Bomb.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bombak
+{
+    class Bomb : Entity
+    {
+        private int radius = 3;
+        private Color color = Color.PaleVioletRed;
+        private double delay = 3.0;
+
+        public Bomb(Point position)
+        {
+            this.position = position;
+            Size computedSize = new Size(this.size, this.size);
+            this.rect = new Rectangle(this.position, computedSize);
+        }
+
+        public override void Draw(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(this.color), rect);
+        }
+    }
+}
---
+++ Bombak/Bombak/Entity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bombak
+{
+    abstract class Entity
+    {
+        protected Point position;
+        protected int size;
+        protected Color color;
+        protected Rectangle rect;
+
+        public Entity()
+        {
+
+        }
+
+        public abstract void Draw(Graphics g);
+    }
+}
