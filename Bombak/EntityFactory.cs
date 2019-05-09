726c460e39988d6e53e37b318647f9edf7ed4119
Ondřej Hliba <hliba.ondra@live.com>
2019-05-09 08:23:45 +0200

    Add Factory

---
+++ Bombak/EntityFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bombak
+{
+    class EntityFactory
+    {
+        private Random r;
+        private Point mousePosition = new Point();
+        public Point MousePosition { get; set; }
+
+        public EntityFactory()
+        {
+            r = new Random();
+        }
+
+        public Entity GenerateEntity(string entityType)
+        {
+            switch (entityType)
+            {
+                case "RUNNER":
+                    return new Runner();
+                case "BOMB":
+                    return new Bomb(mousePosition);
+                default:
+                    return null;
+            }
+        }
+    }
+}
