2334ccd2420aa13c2d65cf18258712632de36aa0
Ondřej Hliba <hliba.ondra@live.com>
2019-05-10 11:29:42 +0200

    Add Randomly Running Runners

--- Bombak/EntityFactory.cs
+++ Bombak/EntityFactory.cs
@@ -9,6 +9,7 @@ namespace Bombak
 {
     class EntityFactory
     {
+        private static Random r = new Random();
         private Point mousePosition = new Point();
         public Point MousePosition
         {
@@ -21,17 +22,24 @@ namespace Bombak
 
         }
 
-        public Entity GenerateEntity(string entityType,Point p)
+        public Entity GenerateEntity(string entityType)
         {
             switch (entityType)
             {
                 case "RUNNER":
-                    return new Runner(p);
+                    return createNewRunner();
                 case "BOMB":
                     return new Bomb(mousePosition);
                 default:
                     return null;
             }
+        }
+
+        private Runner createNewRunner()
+        {
+            int x = Math.Min(r.Next(0, Settings.Instance.fieldSize.Width) * Settings.Instance.cellSize.Width, Settings.Instance.fieldSizePx.Width);
+            int y = Math.Min(r.Next(0, Settings.Instance.fieldSize.Height) * Settings.Instance.cellSize.Height, Settings.Instance.fieldSizePx.Height);
+            return new Runner(new Point(x, y));
         }
     }
 }
--- Bombak/Settings.cs
+++ Bombak/Settings.cs
@@ -12,6 +12,7 @@ namespace Bombak
         public static Settings Instance = new Settings();
         public Size fieldSize = new Size();
         public Size fieldSizePx = new Size();
+        public Size cellSize = new Size();
         public int speed;
 
         public Settings() { }
